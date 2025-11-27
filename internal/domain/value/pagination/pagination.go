4688ac3a20aec2b79f36f217ec29b1753a12ee6f
Stanislav Zeman <zeman@standa.dev>
2025-11-27 11:22:43 +0100

    feat(domain): add pagination value

--- internal/domain/value/pagination/pagination.go
+++ internal/domain/value/pagination/pagination.go
@@ -1,6 +1,29 @@
 package pagination
 
-type Pagination struct {
-	Offset int
-	Limit  int
+type Pagination[T any] struct {
+	offset int
+	limit  int
+}
+
+func New[T any](offset, limit int) Pagination[T] {
+	return Pagination[T]{
+		offset: offset,
+		limit:  limit,
+	}
+}
+
+func (p *Pagination[T]) Paginate(slice []T) []T {
+	if p.offset >= len(slice) || p.limit == 0 {
+		return make([]T, 0)
+	}
+
+	if p.offset > 0 && p.offset < len(slice) {
+		return slice[p.offset:]
+	}
+
+	if p.limit > 0 && p.limit < len(slice) {
+		return slice[:p.limit]
+	}
+
+	return slice
 }
