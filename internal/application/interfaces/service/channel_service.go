b936a3495c89b37a6202fc7d8dd45b72370376bb
Stanislav Zeman <zeman@standa.dev>
2025-11-27 11:20:38 +0100

    feat(application): define service interfaces

--- internal/application/interfaces/service/channel_service.go
+++ internal/application/interfaces/service/channel_service.go
@@ -1,3 +1,13 @@
 package service
 
-type ChannelService interface{}
+import (
+	"github.com/course-go/chanoodle/internal/application/command"
+	"github.com/course-go/chanoodle/internal/application/query"
+)
+
+type ChannelService interface {
+	Channels(q query.Channels) (r query.ChannelsResult, err error)
+	Channel(q query.Channel) (r query.ChannelResult, err error)
+	CreateChannel(c command.CreateChannel) (r command.CreateChannelResult, err error)
+	UpdateChannel(c command.UpdateChannel) (r command.UpdateChannelResult, err error)
+}
--- internal/application/interfaces/service/event_service.go
+++ internal/application/interfaces/service/event_service.go
@@ -1,3 +1,13 @@
 package service
 
-type EventService interface{}
+import (
+	"github.com/course-go/chanoodle/internal/application/command"
+	"github.com/course-go/chanoodle/internal/application/query"
+)
+
+type EventService interface {
+	Events(q query.Events) (r query.EventsResult, err error)
+	Event(q query.Event) (r query.EventResult, err error)
+	CreateEvent(c command.CreateEvent) (r command.CreateEventResult, err error)
+	UpdateEvent(c command.UpdateEvent) (r command.UpdateEventResult, err error)
+}
