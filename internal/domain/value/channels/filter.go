574932185a47dc296ac95834c3161a2be3920592
Stanislav Zeman <zeman@standa.dev>
2025-11-27 11:22:32 +0100

    feat(domain): add filter values

--- internal/domain/value/channels/filter.go
+++ internal/domain/value/channels/filter.go
@@ -1,3 +1,25 @@
 package channels
 
-type Filter struct{}
+import (
+	"slices"
+
+	"github.com/course-go/chanoodle/internal/domain/entity"
+	"github.com/course-go/chanoodle/internal/domain/value/genre"
+)
+
+type Filter struct {
+	Genres []genre.Genre
+}
+
+// Filter runs filtering on [entity.Channel].
+// It returns true when the channel passes the filter.
+func (f *Filter) Filter(channel entity.Channel) bool {
+	// All genres match.
+	for _, genre := range f.Genres {
+		if !slices.Contains(channel.Genres, genre) {
+			return false
+		}
+	}
+
+	return true
+}
--- internal/domain/value/events/filter.go
+++ internal/domain/value/events/filter.go
@@ -1,3 +1,42 @@
 package events
 
-type Filter struct{}
+import (
+	"slices"
+	"time"
+
+	"github.com/course-go/chanoodle/internal/domain/entity"
+	"github.com/course-go/chanoodle/internal/domain/value/genre"
+	"github.com/course-go/chanoodle/internal/domain/value/id"
+)
+
+type Filter struct {
+	Channels []id.ID
+	Genres   []genre.Genre
+	From     *time.Time
+	To       *time.Time
+}
+
+// Filter runs filtering on [entity.Event].
+// It returns true when the event passes the filter.
+func (f *Filter) Filter(event entity.Event) bool {
+	if len(f.Channels) != 0 && !slices.Contains(f.Channels, event.Channel) {
+		return false
+	}
+
+	// All genres match.
+	for _, genre := range f.Genres {
+		if !slices.Contains(event.Genres, genre) {
+			return false
+		}
+	}
+
+	if f.From != nil && f.From.Before(event.From) {
+		return false
+	}
+
+	if f.To != nil && f.To.After(event.From) {
+		return false
+	}
+
+	return true
+}
