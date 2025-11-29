841f3bc41fa9bb270678a1ee100493335b1bdfe5
Stanislav Zeman <zeman@standa.dev>
2025-11-29 23:42:30 +0100

    feat(domain): define service interfaces

--- internal/domain/interfaces/service/event_service.go
+++ internal/domain/interfaces/service/event_service.go
@@ -1,3 +1,15 @@
 package service
 
-type EventService interface{}
+import (
+	"github.com/course-go/chanoodle/internal/domain/entity"
+	"github.com/course-go/chanoodle/internal/domain/value/events"
+	"github.com/course-go/chanoodle/internal/domain/value/id"
+	"github.com/course-go/chanoodle/internal/domain/value/pagination"
+)
+
+type EventService interface {
+	Events(filter events.Filter, pagination pagination.Pagination[entity.Event]) ([]entity.Event, error)
+	Event(id id.ID) (entity.Event, error)
+	CreateEvent(anonymousEvent entity.AnonymousEvent) (entity.Event, error)
+	UpdateEvent(event entity.Event) error
+}
