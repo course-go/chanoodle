d6e43fea99d2abf348d72870e45ce6ff6defc99a
Stanislav Zeman <zeman@standa.dev>
2025-11-29 23:12:02 +0100

    feat(application): implement application services

--- internal/application/service/channel_service.go
+++ internal/application/service/channel_service.go
@@ -1,21 +1,69 @@
 package service
 
 import (
+	"github.com/course-go/chanoodle/internal/application/command"
 	"github.com/course-go/chanoodle/internal/application/interfaces/service"
+	"github.com/course-go/chanoodle/internal/application/query"
+	"github.com/course-go/chanoodle/internal/domain/entity"
 	domain "github.com/course-go/chanoodle/internal/domain/interfaces/service"
+	"github.com/course-go/chanoodle/internal/domain/value/pagination"
 	"github.com/rs/zerolog"
 )
 
-var _ service.EventService = &EventService{}
+var _ service.ChannelService = &ChannelService{}
 
 type ChannelService struct {
 	log            zerolog.Logger
 	channelService domain.ChannelService
 }
 
-func NewChannelService(log zerolog.Logger, channelService domain.ChannelService) ChannelService {
-	return ChannelService{
+func NewChannelService(log zerolog.Logger, channelService domain.ChannelService) *ChannelService {
+	return &ChannelService{
 		log:            log.With().Str("component", "application/channel-service").Logger(),
 		channelService: channelService,
 	}
+}
+
+// Channel implements [service.ChannelService].
+func (cs *ChannelService) Channel(q query.Channel) (r query.ChannelResult, err error) {
+	channel, err := cs.channelService.Channel(q.ID)
+	if err != nil {
+		return r, err
+	}
+
+	r.Channel = channel
+	return r, nil
+}
+
+// Channels implements [service.ChannelService].
+func (cs *ChannelService) Channels(q query.Channels) (r query.ChannelsResult, err error) {
+	pag := pagination.New[entity.Channel](0, 0)
+	channels, err := cs.channelService.Channels(q.Filter, pag)
+	if err != nil {
+		return r, err
+	}
+
+	r.Channels = channels
+	return r, nil
+}
+
+// CreateChannel implements [service.ChannelService].
+func (cs *ChannelService) CreateChannel(c command.CreateChannel) (r command.CreateChannelResult, err error) {
+	channel, err := cs.channelService.CreateChannel(c.Channel)
+	if err != nil {
+		return r, err
+	}
+
+	r.Channel = channel
+	return r, nil
+}
+
+// UpdateChannel implements [service.ChannelService].
+func (cs *ChannelService) UpdateChannel(c command.UpdateChannel) (r command.UpdateChannelResult, err error) {
+	err = cs.channelService.UpdateChannel(c.Channel)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
 }
--- internal/application/service/event_service.go
+++ internal/application/service/event_service.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"github.com/course-go/chanoodle/internal/application/command"
 	"github.com/course-go/chanoodle/internal/application/interfaces/service"
+	"github.com/course-go/chanoodle/internal/application/query"
+	"github.com/course-go/chanoodle/internal/domain/entity"
 	domain "github.com/course-go/chanoodle/internal/domain/interfaces/service"
+	"github.com/course-go/chanoodle/internal/domain/value/pagination"
 	"github.com/rs/zerolog"
 )
 
@@ -13,9 +17,53 @@ type EventService struct {
 	eventService domain.EventService
 }
 
-func NewEventService(log zerolog.Logger, eventService domain.EventService) EventService {
-	return EventService{
+func NewEventService(log zerolog.Logger, eventService domain.EventService) *EventService {
+	return &EventService{
 		log:          log.With().Str("component", "application/event-service").Logger(),
 		eventService: eventService,
 	}
+}
+
+// CreateEvent implements [service.EventService].
+func (es *EventService) CreateEvent(c command.CreateEvent) (r command.CreateEventResult, err error) {
+	event, err := es.eventService.CreateEvent(c.Event)
+	if err != nil {
+		return r, err
+	}
+
+	r.Event = event
+	return r, nil
+}
+
+// Event implements [service.EventService].
+func (es *EventService) Event(q query.Event) (r query.EventResult, err error) {
+	event, err := es.eventService.Event(q.ID)
+	if err != nil {
+		return r, err
+	}
+
+	r.Event = event
+	return r, nil
+}
+
+// Events implements [service.EventService].
+func (es *EventService) Events(q query.Events) (r query.EventsResult, err error) {
+	pag := pagination.New[entity.Event](0, 0)
+	events, err := es.eventService.Events(q.Filter, pag)
+	if err != nil {
+		return r, err
+	}
+
+	r.Events = events
+	return r, nil
+}
+
+// UpdateEvent implements [service.EventService].
+func (es *EventService) UpdateEvent(c command.UpdateEvent) (r command.UpdateEventResult, err error) {
+	err = es.eventService.UpdateEvent(c.Event)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
 }
