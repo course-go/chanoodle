a800baccf3cc2bfc9bb334f2fade483367b09768
Stanislav Zeman <zeman@standa.dev>
2025-11-29 23:42:49 +0100

    feat(domain): implement services

--- internal/domain/service/channel_service.go
+++ internal/domain/service/channel_service.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"github.com/course-go/chanoodle/internal/domain/entity"
 	"github.com/course-go/chanoodle/internal/domain/interfaces/repository"
 	"github.com/course-go/chanoodle/internal/domain/interfaces/service"
+	"github.com/course-go/chanoodle/internal/domain/value/channels"
+	"github.com/course-go/chanoodle/internal/domain/value/id"
+	"github.com/course-go/chanoodle/internal/domain/value/pagination"
 	"github.com/rs/zerolog"
 )
 
@@ -13,9 +17,32 @@ type ChannelService struct {
 	channelRepository repository.ChannelRepository
 }
 
-func NewChannelService(log zerolog.Logger, channelRepository repository.ChannelRepository) ChannelService {
-	return ChannelService{
+func NewChannelService(log zerolog.Logger, channelRepository repository.ChannelRepository) *ChannelService {
+	return &ChannelService{
 		log:               log.With().Str("component", "domain/channel-service").Logger(),
 		channelRepository: channelRepository,
 	}
+}
+
+// Channels implements [service.ChannelService].
+func (cs *ChannelService) Channels(
+	filter channels.Filter,
+	pag pagination.Pagination[entity.Channel],
+) ([]entity.Channel, error) {
+	return cs.channelRepository.Channels(filter, pag)
+}
+
+// Channel implements [service.ChannelService].
+func (cs *ChannelService) Channel(id id.ID) (entity.Channel, error) {
+	return cs.channelRepository.Channel(id)
+}
+
+// CreateChannel implements [service.ChannelService].
+func (cs *ChannelService) CreateChannel(anonymousChannel entity.AnonymousChannel) (entity.Channel, error) {
+	return cs.channelRepository.CreateChannel([]entity.AnonymousChannel{anonymousChannel})
+}
+
+// UpdateChannel implements [service.ChannelService].
+func (cs *ChannelService) UpdateChannel(channel entity.Channel) error {
+	return cs.channelRepository.UpdateChannel(channel)
 }
--- internal/domain/service/event_service.go
+++ internal/domain/service/event_service.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"github.com/course-go/chanoodle/internal/domain/entity"
 	"github.com/course-go/chanoodle/internal/domain/interfaces/repository"
 	"github.com/course-go/chanoodle/internal/domain/interfaces/service"
+	"github.com/course-go/chanoodle/internal/domain/value/events"
+	"github.com/course-go/chanoodle/internal/domain/value/id"
+	"github.com/course-go/chanoodle/internal/domain/value/pagination"
 	"github.com/rs/zerolog"
 )
 
@@ -13,9 +17,29 @@ type EventService struct {
 	eventRepository repository.EventRepository
 }
 
-func NewEventService(log zerolog.Logger, eventRepository repository.EventRepository) EventService {
-	return EventService{
+func NewEventService(log zerolog.Logger, eventRepository repository.EventRepository) *EventService {
+	return &EventService{
 		log:             log.With().Str("component", "domain/event-service").Logger(),
 		eventRepository: eventRepository,
 	}
+}
+
+// Events implements [service.EventService].
+func (es *EventService) Events(filter events.Filter, pag pagination.Pagination[entity.Event]) ([]entity.Event, error) {
+	return es.eventRepository.Events(filter, pag)
+}
+
+// Event implements [service.EventService].
+func (es *EventService) Event(id id.ID) (entity.Event, error) {
+	return es.eventRepository.Event(id)
+}
+
+// CreateEvent implements [service.EventService].
+func (es *EventService) CreateEvent(anonymousEvent entity.AnonymousEvent) (entity.Event, error) {
+	return es.eventRepository.CreateEvent(anonymousEvent)
+}
+
+// UpdateEvent implements [service.EventService].
+func (es *EventService) UpdateEvent(event entity.Event) error {
+	return es.eventRepository.UpdateEvent(event)
 }
